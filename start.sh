a76655f3064dbfc1d65c2051f1d8fd89147c1720
Vanam2002 <117515657+Vanam2002@users.noreply.github.com>
2023-06-23 13:52:40 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Dsdstam/LazyPrincessV2 /LazyPrincessV2
+  git clone https://github.com/Vanam2002/Lazy-Princess-mask /LazyPrincessV2
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /LazyPrincessV2
